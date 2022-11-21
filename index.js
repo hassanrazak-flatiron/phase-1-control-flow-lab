fe9077ba925bc566d0197fd9ff909cdaf5e6ee62
Hassan Razak <hassanrazak1991@gmail.com>
2022-11-20 21:54:58 -0500

    commit tree 19f0a92059a68b7a2a7137689b548a20b04a39f2

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+
+function scuberGreetingForFeet(somevalue){
+ let message;
+  if (somevalue <= 400){
+    message = 'This one is on me!';
+    
+  } else if (somevalue >= 400 && somevalue <= 2000){
+    message = 'That will be twenty bucks.';
+    
+  } else if (somevalue > 2000 && somevalue <2500){
+    message = 'I will gladly take your thirty bucks.';
+    } else if (somevalue >2500){
+      message = 'No can do.'
+    }
+    return message
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(someCity){
+  return (someCity == 'NYC'? "Ok, sounds good." : "No go.")
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let message;
+  switch(tip){
+    case ('generous'):
+      return message = 'Thank you so much.';
+    case ('not as generous'):
+      return message = 'Thank you.';
+    case ('thanks for everything'):
+      return message = 'Bye.'; 
 }
+
+}
+
